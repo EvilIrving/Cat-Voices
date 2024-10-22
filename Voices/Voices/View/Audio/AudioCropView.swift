282dca449e9b443739e443ad3fc75e17f3ffcf45
Garcia <dong.yy1916@gmail.com>
2024-10-22 16:05:30 +0800

    delete: 删除Voices 和 TabDemo

--- Voices/Voices/View/Audio/AudioCropView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  AudioCropView.swift
-//  Voices
-//
-//  Created by Actor on 2024/9/24.
-//
-
-import Foundation
--- Voices/Voices/View/Page/WelcomeView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  WelcomeView.swift
-//  Voices
-//
-//  Created by Actor on 2024/9/24.
-//
-// 欢迎页
-import Foundation
